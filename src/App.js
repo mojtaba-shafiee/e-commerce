b157c163bd8c5c042a43df29fa0dc6ed5eb734c9
mojtaba-see <mojtaba.see@gmial.com>
2020-02-06 17:42:38 +0330

    config react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React from 'react'
 
 import HomePage from './pages/homepage/homepage.component'
 
+import { Route, Switch } from 'react-router-dom'
+
 import './App.css'
+
+const Mens = () => (
+  <div>
+    <h1>Mens Us</h1>
+  </div>
+)
+const Shop = () => (
+  <div>
+    <h1>Shop it!</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/mens' component={Mens} />
+          <Route path='/about/shop' component={Shop} />
+        </Switch>
     </div>
   );
 }
